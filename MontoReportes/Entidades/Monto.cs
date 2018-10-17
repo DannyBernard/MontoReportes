8c5ec56fac3841918c49272557e661bf24269ec3
Danny Bernard <Danny Bernard@DESKTOP-ITSUOPV>
2018-10-17 19:11:00 -0400

    Empezamos

---
+++ MontoReportes/Entidades/Monto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MontoReportes.Entidades
+{
+  public  class Monto
+    {
+        [Key]
+        public int MontoId { get; set; }
+        public string Descripsion { get; set; }
+        public string Tipo { get; set; }
+        public float monto { get; set; }
+
+
+        public Monto()
+        {
+            MontoId = 0;
+            Descripsion = string.Empty;
+            Tipo = string.Empty;
+            monto = 0;
+        }
+    }
+}
