36a0e6877da42dda8dec5cb30f1760d23a3ad47b
Danny Bernard <Danny Bernard@DESKTOP-ITSUOPV>
2018-10-18 11:56:13 -0400

    Seguimos

---
+++ MontoReportes/DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using MontoReportes.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MontoReportes.DAL
+{
+    class Contexto : DbContext
+    {
+        public DbSet<Monto> Montos { get; set; }
+
+        public Contexto() : base("ConStr")
+        {
+
+        }
+    }
+}
---
+++ MontoReportes/Entidades/TipoDeCuentas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MontoReportes.Entidades
+{
+    class TipoDeCuentas
+    {
+        [Key]
+        public int TipoID { get; set; }
+        public string Descripcion { get; set; }
+
+        public TipoDeCuentas()
+        {
+            TipoID = 0;
+            Descripcion = string.Empty;
+        }
+    }
+}
